df2e1e81cd7c92ff244ce0a60467c243e150723e
Valexr <valexr@gmail.com>
2024-05-20 08:53:17 +0200

    fix: quote random

--- src/lib/counters.ts
+++ src/lib/counters.ts
@@ -3,25 +3,25 @@ import { dates } from './dates';
 import { quotes } from './quotes';
 import { DateTime, Interval } from "luxon";
 
-export const counters = derived<[Writable<StartDate[]>, Writable<Quote[]>], Counter[]>(([dates, quotes]),
-    ([$dates, $quotes], set) => {
+export const counters = derived<Writable<StartDate[]>, Counter[]>(dates, ($dates, set) => {
 
-        set($dates.map(({ start, title, quote }, id) => count(id, start, title, quote)))
+    set($dates.map(({ start, title }, id) => count(id, start, title)))
 
-        function count(id: number, start: string, title: string, quote: Quote) {
-            const { years, months, weeks, days, interval } = elapse(start)
-            return {
-                id, title, quote: quote || quotes.random(id), start: start,
-                years, months, days, weeks,
-                full: {
-                    months: Math.trunc(interval.length('months')),
-                    weeks: Math.trunc(interval.length('weeks')),
-                    days: Math.trunc(interval.length('days')),
-                    hours: Math.trunc(interval.length('hours')),
-                }
+    function count(id: number, start: string, title: string) {
+        const { years, months, weeks, days, interval } = elapse(start)
+        return {
+            id, title, start,
+            quote: quotes.random(id),
+            years, months, days, weeks,
+            full: {
+                months: Math.trunc(interval.length('months')),
+                weeks: Math.trunc(interval.length('weeks')),
+                days: Math.trunc(interval.length('days')),
+                hours: Math.trunc(interval.length('hours')),
             }
         }
-    }, [])
+    }
+}, [])
 
 function elapse(start: string) {
     const TO = DateTime.now();
